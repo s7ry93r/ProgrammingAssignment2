3d251a503638970c9f0d5072575e0ac84f3ce7d0
rich <rkrueger@gmail.com>
2014-07-24 11:37:58 -0400

    filled in code for makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix ... will hold a matrix in cache
+## in combination with cacheSolve it will hold the inverted matrix as well
+## what is really returned by this function, is a list of functions 
+## that allow you to access the cached values
 
-## Write a short comment describing this function
+## usage: mtx <- makeCacheMatrix()
+## mtx$set( matrix(c(2,5,-4,5),2,2) )
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #x to hold matrix
+  #inv to hold inverted matrix
+  inv <- NULL
+  setinverse <- function(invmtx) inv <<- invmtx
+  getinverse <- function() inv
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This will take in one of the objects created by makeCacheMatrix and
+## will determine if the inverse has been 'solved' yet and if not it will
+## 'solve' and cache the results and show them, otherwise it will just
+## pull the cached inverse and show it
+## usage looks like this: cacheSolve(mtx)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
